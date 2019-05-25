bdc863040bbae177d5b9260319263b14330a8e98
Clement_Guichard <clement.guichard@viacesi.fr>
2019-05-25 11:53:20 +0200

    Fist push
    init

---
+++ SOS/Assets/Scripts/Physics/Environnement/Env Info/EnvironnementInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable, CreateAssetMenu(fileName = "New Environnement Info", menuName = "Environnement/Environnement Info")]
+public class EnvironnementInfo : ScriptableObject
+{
+    
+
+}
---
+++ SOS/Assets/Scripts/Physics/Gravity/Gravity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class Gravity {
+
+    public static Vector2 gravityDirection = Vector2.down;
+    /* Dans quelle direction va s'appliquer la gravité
+     * Sera toujours normalisée afin de permettre de garder une gravité uniforme dans n'importe quelle direction
+     */
+    
+    public static float gravityAmplitude = 9.81f;
+    /* La force de gravité appliquée sur le joueur (de base, 9.81f)
+     * La gravité appliquée sera gravityDirection.normalized * gravityAmplitude
+     */
+
+    public enum GravityOrientation { Down, Right, Up, Left, Free };
+    public static GravityOrientation gravityOrientation;
+    /* Nous permettra de changer l'orientation de gravityDirection
+     * "Free" nous permettra d'assigner la valeur de gravityDirection comme on le souhaitera
+     */
+
+    public enum GravityInterpolation { Immediate, Lerp, Slerp, Custom };
+    public static GravityInterpolation gravityInterpolation;
+    /* Nous permettra de modifier la transition d'un changement de gravité à un autre
+     * "Custom" nous permettra d'assigner une AnimationCurve ou autre facteur de transition pour avoir la transition souhaitée
+     */
+
+
+}
